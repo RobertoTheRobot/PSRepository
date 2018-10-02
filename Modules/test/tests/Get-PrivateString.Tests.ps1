1921ee043fc579844fdb77dc7c1ea192d71ff900
Roberto <roberto.perezaldanas@gmail.com>
2018-10-02 14:32:32 +0200

    Create Get-PrivateString.Tests.ps1

---
+++ Modules/test/tests/Get-PrivateString.Tests.ps1
@@ -0,0 +1,8 @@
+. "$PSScriptRoot\..\src\Public\Get-PrivateString.ps1"
+
+Describe "Get-PrivateString" {
+    It "It returns a test string" {
+        $test_object = $null
+        $test_collection | Should Be $null
+    }
+}
